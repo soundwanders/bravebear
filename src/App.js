e352a7b767ce047e612db9cd03ec9633bac2d188
soundwanders <soundwanders@gmail.com>
2021-06-07 21:42:47 -0400

    redbear html template css grid, rough draft setup

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ import vestBear from './bears/vestBear.png';
 import './App.css';
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <h3>Brave Bear</h3>
-      </header>
-        <body>
-          <p>
-              <span>
-                <img src={redBear} className="App-bears" alt="Red Bear"/>
-              </span>
+  <div className="App">
+    <header className="App-header">
+      <h3>Brave Bear</h3>
+    </header>
+      <body>
+        <p>
+            <span>
+              <img src={swimBear} className="App-bears" alt="Swim Bear"/>
+            </span>
 
-              <span>
-                <img src={swimBear} className="App-bears" alt="Swim Bear"/>
-              </span>
+            <span>
+              <img src={vestBear} className="App-bears" alt="Vest Bear"/>
+            </span>
 
-              <span>
-                <img src={overallBear} className="App-bears" alt="Overalls Bear"/>
-              </span>
-
-              <span>
-                <img src={vestBear} className="App-bears" alt="Vest Bear"/>
-              </span>
-            </p>
-          </body>
-    </div>
-  );
+            <span>
+              <img src={overallBear} className="App-bears" alt="Overalls Bear"/>
+            </span>
+            
+            <span>
+              <img src={redBear} className="App-bears" alt="Red Bear"/>
+            </span>
+          </p>
+        </body>
+  </div>
+);
 
 export default App;
