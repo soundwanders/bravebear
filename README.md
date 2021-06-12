3b7a246110d62e1051adbd1ffe5df162002a7c3e
soundwanders <soundwanders@gmail.com>
2021-06-12 16:37:32 -0400

    redbear work

--- README.md
+++ README.md
@@ -68,3 +68,17 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+&nbsp;
+
+
+### Image Accreditations
+
+&nbsp;
+
+#### From https://www.flaticon.com ...
+- https://www.flaticon.com/authors/eucalyp
+
+- https://www.flaticon.com/authors/phatplus
+
+- https://www.flaticon.com/authors/freepik
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import overallBear from './bears/overallBear.png';
-import redBear from './bears/redBear.png';
-import swimBear from './bears/swimBear.png';
-import vestBear from './bears/vestBear.png';
+import overallBear from './media/bears/overallBear.png';
+import redBear from './media/bears/redBear.png';
+import swimBear from './media/bears/swimBear.png';
+import vestBear from './media/bears/vestBear.png';
 
 import './App.css';
 
@@ -15,19 +15,19 @@ const App = () => (
       <body>
         <p>
             <span>
-              <img src={swimBear} className="App-bears" alt="Swim Bear"/>
+              <img src={swimBear} className="bear swim" alt="Swim Bear"/>
             </span>
 
             <span>
-              <img src={vestBear} className="App-bears" alt="Vest Bear"/>
+              <img src={vestBear} className="bear vest" alt="Vest Bear"/>
             </span>
 
             <span>
-              <img src={overallBear} className="App-bears" alt="Overalls Bear"/>
+              <img src={overallBear} className="bear overalls" alt="Overalls Bear"/>
             </span>
             
             <span>
-              <img src={redBear} className="App-bears" alt="Red Bear"/>
+              <img src={redBear} className="bear red" alt="Red Bear"/>
             </span>
           </p>
         </body>
