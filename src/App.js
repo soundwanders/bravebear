6fe2ca8293d0398617d923e2058de064364b6fd2
soundwanders <soundwanders@gmail.com>
2021-06-27 20:31:13 -0400

    editing components, setting up react-router to switch between templates. Need to render bluebear UI on click, then expand from there

--- src/App.js
+++ src/App.js
@@ -1,37 +1,125 @@
-import React from 'react'
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
-import overallBear from './media/bears/overallBear.png';
-import redBear from './media/bears/redBear.png';
-import swimBear from './media/bears/swimBear.png';
-import blueBear from './media/bears/blueBear.png';
+// import brownBearImage from './media/bears/brownBear.png';
+// import redBearImage from './media/bears/redBear.png';
+import blueBearImage from './media/bears/blueBear.png';
+import './index.css';
+import './blueBear-template/blue.css';
 
-import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h3>Brave Bear</h3>
-    </header>
-      <body>
-        <p>
-            <span>
-              <img src={swimBear} className="bear swim" alt="Swim Bear"/>
-            </span>
+export class BlueBearTemplate extends React.Component {
+  render() {
+    return (
+      <main>
+        <header>
+          <nav>
+            <div id="navbar">
+              <ul>
+                <li class="link 1"><Link to="/">Home</Link></li>
+                <li class="link 2"><Link to="bluebear">Blue Bear</Link></li>
+                <li class="link 3"><a href="#brownbear">Brown Bear</a></li>
+                <li class="link 4"><a href="#redbear">Red Bear</a></li>
+              </ul>
+              </div>
+          </nav>
+        </header> 
 
-            <span>
-              <img src={blueBear} className="bear blue" alt="Blue Bear"/>
-            </span>
+        <div class="wrapper">
+          <div class="grid-container">
+            <div class="item1">
+              <p id="bunchabears">
+                <img class="bear-logo" id="rb" src="/src/media/bears/redBear.png" alt="A Brave Red Bear"/>
+                <img class="bear-logo" src="/src/media/bears/brownBear.png" alt="A Brave Brown Bear"/>
+                <img class="bear-logo" src="/src/media/bears/blueBear.png" alt="A Brave Blue Bear"/>
+              </p>
+            </div>
 
-            <span>
-              <img src={overallBear} className="bear overalls" alt="Overalls Bear"/>
-            </span>
-            
-            <span>
-              <img src={redBear} className="bear red" alt="Red Bear"/>
-            </span>
-          </p>
-        </body>
-  </div>
-);
 
-export default App;
+            <div class="item2">
+              <div class="content-title"><h3>Brave Bear Blue</h3></div>
+
+              <div class="content-container">
+                  <div class="box 1">
+                    <p>App Development</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/appDev.svg" alt="Application Development"/>
+                      </div>
+                  </div>
+
+                  <div class="box 2">
+                    <p>Cloud Storage</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/cloud.svg" alt="Cloud Storage"/>
+                      </div>
+                  </div>
+
+                  <div class="box 3">
+                    <p>CMS Software</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/cms.svg" alt="CMS Software"/>
+                      </div>
+                  </div>
+
+                  <div class="box 4">
+                    <p>E-Commerce</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/ecommerce.svg" alt="E-Commerce Builds"/>
+                      </div>
+                  </div>
+
+                  <div class="box 5">
+                    <p>Server Hosting</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/database.svg" alt="Server Hosting"/>
+                      </div>
+                  </div>
+
+                  <div class="box 6">
+                    <p>Site Maintenance</p>
+                      <div class="icon-container">
+                        <img class="icons" src="/src/media/icons/maintenance.svg" alt="Site Maintenance"/>
+                      </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="item3">
+              <img class="polar" src="/src/media/bears/polar-beige.png" alt="Peeking Polar Bear"/>
+            </div>
+
+            <div class="item4">
+              <p>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><img class ="footer-logo" src="/src/media/icons/facebook.svg" alt="Facebook"/></a>
+                <a href="https://instagram.com" target="_blank" rel="noreferrer"><img class="footer-logo" src="/src/media/icons/instagram.svg" alt="Instagram"/></a>
+                <a href="https://github.com" target="_blank" rel="noreferrer"><img class="footer-logo" src="/src/media/icons/github-bw.svg" alt="Github"/></a>
+                <a href="https://stackoverflow.com" target="_blank" rel="noreferrer"><img class="footer-logo" src="/src/media/icons/stack-overflow.svg" alt="Stack Overflow"/></a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </main>
+    )
+  }
+}
+
+class App extends React.Component {
+  nextPath(path) { 
+    this.props.history.push(path);
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="App">
+          <button onClick={() => this.nextPath("bluebear") }>
+                <img src={ blueBearImage } className="bear blue" alt="A Brave Blue Bear"/>
+          </button>
+        </div>
+      </main>
+    )
+  }
+};
+
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from './App';
+import BlueBearTemplate from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render((
+  <Router>
+      <Route exact path = "/" component = {App} />
+      <Route path = "bluebear" component = {BlueBearTemplate} />
+  </Router>
+
+), document.getElementById('root'));
